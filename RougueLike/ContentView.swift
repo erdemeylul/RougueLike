3dd562fff171edaa3446e81051e51acc2f44fa20
erdemeylul <mensuckmankindrocks@gmail.com>
2021-07-23 11:48:36 +0300

    Initial Commit

---
+++ RougueLike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RougueLike
+//
+//  Created by Erdem Senol on 23.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RougueLike/RougueLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RougueLikeApp.swift
+//  RougueLike
+//
+//  Created by Erdem Senol on 23.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct RougueLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
